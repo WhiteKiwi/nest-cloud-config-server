b6f4c07fe81391960d8edf613d6a41ee09a40a12
장지훈 <kiwi.jang@toss.im>
2022-02-27 02:22:51 +0900

    feat(): 배포 관련 셋업

---
+++ test/e2e/app.e2e-spec.ts
@@ -0,0 +1,27 @@
+import { HttpStatus, INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import * as supertest from 'supertest';
+
+import { AppModule } from '../../src/app.module';
+
+describe('AppController (e2e)', () => {
+	let app: INestApplication;
+	let request: supertest.SuperTest<supertest.Test>;
+
+	beforeEach(async () => {
+		const moduleFixture: TestingModule = await Test.createTestingModule({
+			imports: [AppModule],
+		}).compile();
+
+		app = moduleFixture.createNestApplication();
+		await app.init();
+		request = supertest(app.getHttpServer());
+	});
+
+	it('/health-check (GET)', async () => {
+		const { status, text } = await request.get('/health-check');
+
+		expect(status).toBe(HttpStatus.OK);
+		expect(text).toBe('OK');
+	});
+});
