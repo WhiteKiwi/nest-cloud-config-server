ea4b6be6b9a01a5d7820830d99a6865611429f41
장지훈 <kiwi.jang@toss.im>
2022-02-27 01:43:57 +0900

    feat(): config에서 PORT 받아오도록 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { HealthCheckModule } from '@kiwi-lib/nestjs';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { CloudConfigModule } from './modules';
 
 @Module({
-	imports: [HealthCheckModule, CloudConfigModule],
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true,
+			envFilePath: '.env',
+		}),
+		HealthCheckModule,
+		CloudConfigModule,
+	],
 })
 export class AppModule {}
