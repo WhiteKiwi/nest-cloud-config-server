e88b13c1ea992823bf93d5cc7f4760fc9ce29433
장지훈 <kiwi.jang@toss.im>
2022-02-27 01:40:53 +0900

    feat(): HealthCheckModule 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
+import { HealthCheckModule } from '@kiwi-lib/nestjs';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 import { CloudConfigModule } from './modules';
 
 @Module({
-	imports: [CloudConfigModule],
-	controllers: [AppController],
-	providers: [AppService],
+	imports: [HealthCheckModule, CloudConfigModule],
 })
 export class AppModule {}
