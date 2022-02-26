c56d362a2ec80a8895ac07df33a857485a57585f
장지훈 <kiwi.jang@toss.im>
2022-02-27 01:43:57 +0900

    feat(): config에서 PORT 받아오도록 수정

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
+import { Env } from './config/env';
 
-const PORT = 3000;
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 
-	await app.listen(PORT, () => {
-		console.log(`SERVER LISTENING ON ${PORT}`);
+	const configService: ConfigService<Env> = app.get(ConfigService);
+	const port = configService.get<number>('PORT') || 3000;
+
+	await app.listen(port, () => {
+		console.log(`SERVER LISTENING ON port ${port}`);
 	});
 }
 bootstrap();
