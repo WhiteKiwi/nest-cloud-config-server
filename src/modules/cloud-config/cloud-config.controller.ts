30c19342d69cc59e1c921189f482a915fb1c06f2
장지훈 <kiwi.jang@toss.im>
2022-02-27 01:46:38 +0900

    comment(): 주석 추가

--- src/modules/cloud-config/cloud-config.controller.ts
+++ src/modules/cloud-config/cloud-config.controller.ts
@@ -2,11 +2,13 @@ import { Controller, Get, Param } from '@nestjs/common';
 
 import { CloudConfigService } from './cloud-config.service';
 
-@Controller(':configGroup')
+@Controller()
 export class CloudConfigController {
 	constructor(private readonly cloudConfigService: CloudConfigService) {}
 
-	@Get()
+	// TODO: encrypt, decrypt api 추가
+
+	@Get(':configGroup')
 	getConfig(@Param('configGroup') configGroup: string): unknown {
 		return this.cloudConfigService.getConfigValue(configGroup) || {};
 	}
