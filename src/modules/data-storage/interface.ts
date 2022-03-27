de2815fd1752d12669a6ac22e0c9ff722781d02f
장지훈 <kiwi.jang@toss.im>
2022-03-27 14:22:30 +0900

    feat(data-storage): type 추가

--- src/modules/data-storage/interface.ts
+++ src/modules/data-storage/interface.ts
@@ -1,5 +1,16 @@
-export interface DataStorage<T = unknown> {
+export interface DataStorage<T extends KeyValueMapping<any, any> = any> {
 	initialize?(): void | Promise<void>;
-	load(key: string): T | Promise<T> | undefined;
-	save(key: string, value: T): void | Promise<void>;
+	load<K extends KeyType<T>>(key: K): Promise<T[K]> | undefined;
+	save<K extends KeyType<T>>(key: K, value: T[K]): Promise<void>;
 }
+
+type KeyValueMapping<K extends string, V> = {
+	[key in K]: V;
+};
+
+type KeyType<T extends KeyValueMapping<any, any>> = T extends KeyValueMapping<
+	infer K,
+	infer V
+>
+	? K
+	: never;
